4fc59167d5bbc3126a1401518f2940a7b6e94044
Brad Beckwith <brad.beckwith@gmail.com>
2018-10-25 12:17:27 -0700

    Initial load of DomainSlice Python script. This script returns the persons name and E-mail domain.

--- Python/DomainSlice.py
+++ Python/DomainSlice.py
@@ -3,5 +3,5 @@
 email = input("What is your email address?:")
 user = email[:email.index("@")]
 domain = email[email.index("@") + 1:]
-output = "Your username is {} and your domain is {}".format(user,domain)
+output = "Your username is {} and your Email domain is {}".format(user,domain)
 print(output)
