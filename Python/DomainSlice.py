f6808693870f96b3b3d2cd098ceaf9112f763fd1
Brad Beckwith <brad.beckwith@gmail.com>
2018-10-25 10:24:50 -0700

    Created new Python file DomainSlice.py

---
+++ Python/DomainSlice.py
@@ -0,0 +1,7 @@
+# Email Slicer
+
+email = input("What is your email address?:")
+user = email[:email.index("@")]
+domain = email[email.index("@") + 1:]
+output = "Your username is {} and your domain is {}".format(user,domain)
+print(output)
