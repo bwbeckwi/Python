9764ab10341a865c4a1843148c67e060aaec9901
Brad Beckwith <brad.beckwith@gmail.com>
2018-10-30 09:09:47 -0700

    Adding more scripts

--- Python/Test01/is_Server_up.py
+++ Python/Test01/is_Server_up.py
@@ -1,6 +1,25 @@
-import os
+'''
+
+Written by:     Brad Beckwith
+Date:           October 20, 2018
+Purpose:        Learn more about Python
+
+'''
 
 
-name = input("Enter computer name: ")
-print(os.system("ping -t " + name))
+from os import system
+
+
+def ping_host():
+    comp_name = input("Enter computer name: ")
+    num_tries = input("Enter number of retries: ")
+    print(system("ping -n " + num_tries + " " + comp_name))
+
+
+def main():
+    ping_host()
+
+
+if __name__ == '__main__':
+    main()
 
