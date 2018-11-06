a2a91488eac88f188ab49881323ee0af40cb01c2
Brad Beckwith <brad.beckwith@gmail.com>
2018-11-06 07:00:19 -0700

    Changed dog.py while testing.

--- PythonExamples/dog.py
+++ PythonExamples/dog.py
@@ -1,10 +1,18 @@
 class dog:
-    def __init__(self, name, age):
+    def __init__(self, name, age, weight):
         self.name = name
         self.age = age
+        self.weight = weight
+
 
     def bark(self):
-        print("Woof Woof! My name is: " + self.name)
+        print("Woof Woof! My name is: {}, I weigh {} pounds and I am {} years old".format(self.name, str(self.weight), str(self.age)))
+
 
     def walk(self, distance):
-        print("I just walked a distance of: " + str(distance))
+        print("{} just walked a distance of: {}".format(self.name,str(distance)))
+
+
+mydog = dog("Sam", 5, 68)
+
+mydog.walk(distance="1 mile")
